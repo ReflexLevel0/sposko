b4aaf11e35ea6c9bfcfdd9c718245353649181fc
ReflexLevel0 <dominikdejanovic2@gmail.com>
2025-04-12 22:08:08 +0200

    Updated db.sql

--- db.sql
+++ db.sql
@@ -2,11 +2,12 @@ CREATE TABLE personal_info(
   id uuid DEFAULT gen_random_uuid() PRIMARY KEY, 
   first_name VARCHAR(64) NOT NULL,
   last_name VARCHAR(64) NOT NULL,
-  phone_number VARCHAR(20) NOT NULL,
+  phone_number VARCHAR(32),
+  email VARCHAR(64)
 );
 
 CREATE TABLE account(
-  id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  id uuid PRIMARY KEY,
   username VARCHAR(32) NOT NULL UNIQUE,
   password VARCHAR(64) NOT NULL,
   FOREIGN KEY (id) REFERENCES personal_info (id) ON DELETE CASCADE
@@ -33,6 +34,15 @@ CREATE TABLE child(
   FOREIGN KEY (id) REFERENCES personal_info (id) ON DELETE CASCADE
 );
 
+CREATE TABLE note(
+  id SERIAL PRIMARY KEY,
+  trainer_id uuid,
+  child_id uuid,
+  text TEXT,
+  FOREIGN KEY (trainer_id) REFERENCES trainer (trainer_id),
+  FOREIGN KEY (child_id) REFERENCES child (child_id)
+);
+
 CREATE TABLE sport(
   id SERIAL PRIMARY KEY,
   name VARCHAR(64) NOT NULL UNIQUE
@@ -41,26 +51,29 @@ CREATE TABLE sport(
 CREATE TABLE sport_group(
   id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
   name VARCHAR(30) NOT NULL UNIQUE,
-  sport_id SERIAL,
-  FOREIGN KEY (sport_id) REFERENCES sport (id)
+  sport_id SERIAL NOT NULL,
+  max_members INT,
+  min_age INT,
+  max_age INT,
+  FOREIGN KEY (sport_id) REFERENCES sport (id) ON DELETE CASCADE
 );
 
-CREATE TABLE group_child(
+CREATE TABLE child_group(
   group_id uuid,
   child_id uuid,
   PRIMARY KEY (group_id, child_id),
-  FOREIGN KEY (group_id) REFERENCES sport_group (id),
-  FOREIGN KEY (child_id) REFERENCES child (id)
+  FOREIGN KEY (group_id) REFERENCES sport_group (id) ON DELETE CASCADE,
+  FOREIGN KEY (child_id) REFERENCES child (id) ON DELETE CASCADE
 );
 
-CREATE TABLE training_schedule(
+CREATE TABLE training(
   group_id uuid,
-  training_start_date DATE,
-  training_start_time TIME WITH TIME ZONE NOT NULL,
-  training_end_time TIME WITH TIME ZONE NOT NULL,
-  training_end_date DATE DEFAULT NULL,
-  repeat_type VARCHAR(32) DEFAULT NULL,
-  repeat_interval INT DEFAULT NULL,
+  start_date DATE NOT NULL,
+  start_time TIME WITH TIME ZONE NOT NULL,
+  duration INTERVAL NOT NULL,
+  end_date DATE,
+  repeat_type VARCHAR(32),
+  repeat_interval INT,
   PRIMARY KEY(group_id, training_start_date, training_start_time),
-  FOREIGN KEY (group_id) REFERENCES sport_group (id)
+  FOREIGN KEY (group_id) REFERENCES sport_group (id) ON DELETE CASCADE
 );

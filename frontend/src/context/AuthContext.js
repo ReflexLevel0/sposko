51271ef396597efd8791c5c3e9079dfff1124472
crespo77777 <markozagorc1407@gmail.com>
2025-05-21 20:03:29 +0200

    Update AuthContext.js
    maknut user

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -3,23 +3,13 @@ import React, { createContext, useState, useEffect } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    id: 2,
-    first_name: "Test",
-    last_name: "Trener",
-    email: "test@trener.hr",
-    phone_number: "0998765432",
-    role: "trainer",
-    verified: true,
-    date_of_birth: "1990-01-01",
-    info: "Iskusni trener",
-  });
+  const [user, setUser] = useState({null});
 
   // On mount, check for token & user in localStorage
-  /*useEffect(() => {
+  useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) setUser(JSON.parse(storedUser));
-  }, []);*/
+  }, []);
 
   const login = (userData, token) => {
     setUser(userData);
